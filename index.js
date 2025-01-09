7f75a46366382eb95503c1341f0141e27d531bf1
nuralom787 <md9577630@gmail.com>
2025-01-09 20:00:12 +0600

    implement JWT Token

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('dotenv').config()
+const jsonwebtoken = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 
 const port = process.env.PORT || 5000;
 
 // Middleware.
-app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 
 // Service
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kwi75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,6 +31,33 @@ const client = new MongoClient(uri, {
 
 
 
+// Middleware.
+// const logger = async (req, res, next) => {
+//     console.log("Called: ", req.host, req.originalUrl);
+//     next()
+// };
+
+
+// Verify Token.
+const VerifyToken = async (req, res, next) => {
+    const token = req.cookies?.token;
+    // console.log("token from verify token", token);
+    if (!token) {
+        return res.status(401).send({ message: "Unauthorize Access" })
+    };
+    jsonwebtoken.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        // Error.
+        if (err) {
+            return res.status(401).send({ message: "Unauthorize Access" })
+        }
+        // Decoded.
+        req.user = decoded;
+        next();
+    });
+};
+
+
+
 async function run() {
     try {
         // DB CONNECT.
@@ -33,6 +66,30 @@ async function run() {
         // Collections.
         const servicesCollections = client.db("CarDoctor").collection("Services");
         const bookingsCollections = client.db("CarDoctor").collection("Bookings");
+
+
+
+        //-------------------------------------
+        //          Auth Related API.
+        // ------------------------------------
+
+
+        // Create JWT.
+        app.post("/jwt", async (req, res) => {
+            const user = req.body;
+            const token = jsonwebtoken.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+            res.send({ success: true });
+        });
+
+
+
+        // -------------------------------------
+        //          Service Related API.
+        // -------------------------------------
 
 
         // Get All Services.
@@ -64,7 +121,16 @@ async function run() {
 
 
         // Get Bookings Info.
-        app.get("/bookings", async (req, res) => {
+        app.get("/bookings", VerifyToken, async (req, res) => {
+            // console.log("Query Email: ", req.query.email);
+            // console.log("Cookie Token", req.cookies.token);
+            // console.log("User Token info From Bookings", req.user);
+
+            // Verify Data Requested User.
+            if (req.query?.email !== req.user.email) {
+                return res.status(403).send({ message: "Forbidden Access" })
+            }
+
             let query = {};
             if (req.query?.email) {
                 query = { customer_email: req.query.email }
